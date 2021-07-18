b20f2881150089af327fff0dd2a74241644f4fdd
w96086123 <w96086123@gmail.com>
2021-07-18 21:42:47 +0800

    Update searchFile.py
    Use Pandas

--- searchFile.py
+++ searchFile.py
@@ -1,15 +1,15 @@
 from PIL import Image
-from PIL.ExifTags import TAGS
 import os
 import csv
 import numpy
 import time
+import pandas
 
 time_start = time.time()
 
 classList = ['Human face', 'Human eye', 'Human nose', 'Human mouth']
 classCsvFilePath = 'F:\\python\\40-50-OIDv4_ToolKit_download_google_public_images\\OID\\csv_folder\\class-descriptions-boxable.csv'
-labelFolderPath = 'F:\\python\\40-50-OIDv4_ToolKit_download_google_public_images\\OID\\Dataset'
+labelFolderPath = 'C:\\Users\\User\\Desktop\\pythontest'
 annotationsbaseCsvPath = 'F:\\python\\40-50-OIDv4_ToolKit_download_google_public_images\\OID\\csv_folder\\'
 annotationCsv = '-annotations-bbox.csv'
 dataList = ['train', 'validation', 'test']
@@ -27,31 +27,34 @@ def searchClassCode(className):
     return classCode, className2
 
 
-def writeFileData(imageName, row, classCode, className, folderPath, size):
-    with open(folderPath, 'a') as out_file:
-        a = []
-        for i in range(4, 8):
-            if i < 6:
-                a.append(float(row[i])*size[0])
-            else:
-                a.append(float(row[i])*size[1])
-        out_file.write("{} {} {} {} {}\n".format(className[classCode.index(
-            row[2])], a[0], a[2], a[1], a[3]))
+def writeFileData(csv_df, classCode, className, folderPath, size):
+    with open(folderPath, 'w') as out_file:
+        for row in range(csv_df.shape[0]):
+            a = []
+            for i in range(4, 8):
+                if i < 6:
+                    a.append(float(csv_df.iloc[row][i])*size[0])
+                else:
+                    a.append(float(csv_df.iloc[row][i])*size[1])
+            out_file.write("{} {} {} {} {}\n".format(className[classCode.index(
+                csv_df.iloc[row][2])], a[0], a[2], a[1], a[3]))
 
 
 def searchFileData(imageNameList, classCode, className, folderName, secondFolderName):
     dataList = []
     with open(annotationsbaseCsvPath+folderName+annotationCsv, newline='') as df:
-        rows = csv.reader(df)
-        for row in rows:
-            if row[0] in imageNameList and row[2] in classCode:
-                img = Image.open(labelFolderPath+'\\'+folderName +
-                                 '\\'+secondFolderName+'\\'+row[0]+'.jpg')
-                writeFileData(row[0], row, classCode, className, labelFolderPath +
-                              '\\'+folderName+'\\'+secondFolderName+'\\Label\\'+row[0]+'.txt', img.size)
+        csv_data = pandas.read_csv(df)
+        csv_df = pandas.DataFrame(csv_data)
+        for imageName in imageNameList:
+            a = csv_df["ImageID"] == imageName
+            c = csv_df["LabelName"].isin(classCode)
+            b = csv_df[a & c]
+            img = Image.open(labelFolderPath+'\\'+folderName +
+                             '\\'+secondFolderName+'\\'+imageName+'.jpg')
+            writeFileData(b, classCode, className, labelFolderPath +
+                          '\\'+folderName+'\\'+secondFolderName+'\\Label\\'+imageName+'.txt', img.size)
 
 
-imageList = []
 classCode, className = searchClassCode(classList)
 
 os.chdir(labelFolderPath)
@@ -61,6 +64,7 @@ for folderName in folderList:
     if folderName in dataList:
         secondFolderList = os.listdir(labelFolderPath+'\\'+folderName)
         for secondFolderName in secondFolderList:
+            imageList = []
             imageList2 = os.listdir(
                 labelFolderPath+'\\'+folderName+'\\'+secondFolderName)
             for imageName in imageList2:
