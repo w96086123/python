4c762a00305dc427820e79a75d1786539fc62152
w96086123 <w96086123@gmail.com>
2021-07-18 19:20:17 +0800

    Update searchFile.py

--- searchFile.py
+++ searchFile.py
@@ -3,6 +3,9 @@ from PIL.ExifTags import TAGS
 import os
 import csv
 import numpy
+import time
+
+time_start = time.time()
 
 classList = ['Human face', 'Human eye', 'Human nose', 'Human mouth']
 classCsvFilePath = 'F:\\python\\40-50-OIDv4_ToolKit_download_google_public_images\\OID\\csv_folder\\class-descriptions-boxable.csv'
@@ -10,8 +13,6 @@ labelFolderPath = 'F:\\python\\40-50-OIDv4_ToolKit_download_google_public_images
 annotationsbaseCsvPath = 'F:\\python\\40-50-OIDv4_ToolKit_download_google_public_images\\OID\\csv_folder\\'
 annotationCsv = '-annotations-bbox.csv'
 dataList = ['train', 'validation', 'test']
-
-# 查詢編號
 
 
 def searchClassCode(className):
@@ -24,32 +25,30 @@ def searchClassCode(className):
                 classCode.append(row[0])
                 className2.append(row[1])
     return classCode, className2
-# 搜尋各圖片數值
 
 
-def searchFileData(imageNameList, classCode, folderName):
+def writeFileData(imageName, row, classCode, className, folderPath, size):
+    with open(folderPath, 'a') as out_file:
+        a = []
+        for i in range(4, 8):
+            if i < 6:
+                a.append(float(row[i])*size[0])
+            else:
+                a.append(float(row[i])*size[1])
+        out_file.write("{} {} {} {} {}\n".format(className[classCode.index(
+            row[2])], a[0], a[2], a[1], a[3]))
+
+
+def searchFileData(imageNameList, classCode, className, folderName, secondFolderName):
     dataList = []
     with open(annotationsbaseCsvPath+folderName+annotationCsv, newline='') as df:
         rows = csv.reader(df)
         for row in rows:
             if row[0] in imageNameList and row[2] in classCode:
-                dataList.append(row)
-    return dataList
-# 寫入圖片資料
-
-
-def writeFileData(imageName, fileDataList, classCode, className, folderPath, size):
-    with open(folderPath, 'w') as out_file:
-        for row in fileDataList:
-            if row[0] == imageName and row[2] in classCode:
-                a = []
-                for i in range(4, 8):
-                    if i < 6:
-                        a.append(float(row[i])*size[0])
-                    else:
-                        a.append(float(row[i])*size[1])
-                out_file.write("{} {} {} {} {}\n".format(className[classCode.index(
-                    row[2])], a[0], a[2], a[1], a[3]))
+                img = Image.open(labelFolderPath+'\\'+folderName +
+                                 '\\'+secondFolderName+'\\'+row[0]+'.jpg')
+                writeFileData(row[0], row, classCode, className, labelFolderPath +
+                              '\\'+folderName+'\\'+secondFolderName+'\\Label\\'+row[0]+'.txt', img.size)
 
 
 imageList = []
@@ -68,9 +67,8 @@ for folderName in folderList:
                 if imageName != 'Label':
                     imageName = imageName.split('.')[0]
                     imageList.append(imageName)
-                fileDataList = searchFileData(imageList, classCode, folderName)
-            for imageName in imageList:
-                img = Image.open(labelFolderPath+'\\'+folderName +
-                                 '\\'+secondFolderName+'\\'+imageName+'.jpg')
-                writeFileData(imageName, fileDataList, classCode, className, labelFolderPath +
-                              '\\'+folderName+'\\'+secondFolderName+'\\Label\\'+imageName+'.txt', img.size)
+            searchFileData(imageList, classCode, className,
+                           folderName, secondFolderName)
+
+time_end = time.time()
+print('test cost', time_end-time_start, 's')
